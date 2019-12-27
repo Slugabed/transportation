b1082ea7329e34e2cae3a98682e19ebeeaba918a
Булат Радикович Исламов <islamov@phystech.edu>
2019-12-27 22:44:42 +0300

    commit 2

--- main.cpp
+++ main.cpp
@@ -8,69 +8,132 @@
 
 using namespace std;
 
-typedef pair<unsigned , unsigned> Edge;
+typedef pair<unsigned, unsigned> Edge;
+unsigned N, M;
+unsigned DAY_IN_MINUTES = 24 * 60;
+unsigned MAX_TIME = 10 * DAY_IN_MINUTES;
 
-Edge make_edge(unsigned v1, unsigned v2){
-    return minmax(v1,v2);
+Edge make_edge(unsigned v1, unsigned v2) {
+    return make_pair(v1, v2);
 }
-struct Info{
+
+struct Info {
     int time;
-    int bandwidth;
+    int weight;
 };
 typedef map<Edge, Info> Graph;
 
-void sort_and_unique(vector<int>& v){
+void sort_and_unique(vector<int> &v) {
     sort(v.begin(), v.end());
     v.erase(unique(v.begin(), v.end()), v.end());
 }
 
-bool IfPathExists(const Graph& graph, const unsigned& count_vertex, const unsigned& max_weight){
+
+struct VertexDistance {
+    unsigned distance;
+    unsigned vertex;
+};
+
+bool operator<(const VertexDistance &left, const VertexDistance &right) {
+    return left.distance < right.distance;
+};
+
+const auto &HeapComparator = [](const VertexDistance &left, const VertexDistance &right) {
+    return left.distance >= right.distance;
+};
+//const auto &FindVertexDistanceComparator = [](const VertexDistance &left, const VertexDistance &right) {
+//    return
+//}; TODO
+
+unsigned CountDistance(const Graph &graph, const unsigned &count_vertex, const unsigned &max_weight) {
     map<unsigned, unsigned> distance;
-    vector<Edge> heap;
-//    while(!heap.empty()){
-//
-//    }
-    return false;
+    vector<VertexDistance> heap = {{0, 1}};
+    make_heap(heap.begin(), heap.end(), HeapComparator);
+    while (!heap.empty()) {
+        pop_heap(heap.begin(), heap.end(), HeapComparator);
+        VertexDistance current = *(heap.end() - 1);
+        const unsigned &top_vertex = current.vertex;
+        heap.pop_back();
+        // find all adjacent edges to top_vertex from heap
+        const auto &beginIt = graph.lower_bound(make_edge(top_vertex, 1));
+        const auto &endIt = graph.upper_bound(make_edge(top_vertex, N));
+
+        for (auto it = beginIt; it != endIt; it++) {
+            const Edge &edge = it->first;
+            const Info &info = it->second;
+            const unsigned &v2 = edge.second;
+            if (info.weight < max_weight) {
+                continue;
+            }
+            if (distance.count(v2) == 0) {
+                distance[v2] = distance[top_vertex] + info.time;
+                heap.push_back({distance[v2], v2});
+                push_heap(heap.begin(), heap.end());
+                continue;
+            }
+            if (distance[top_vertex] + info.time < distance[v2]) {
+                distance[v2] = distance[top_vertex] + info.time;
+//                binary_search(heap.begin(), heap.end(), {0, v2}, ) TODO
+            }
+            if (v2 == N and distance[v2] <= MAX_TIME) {
+                break;
+            }
+        }
+    }
+    return distance[N];
 }
+
+bool IfPathExists(const Graph &graph, const unsigned &count_vertex, const unsigned &max_weight){
+    return CountDistance(graph, count_vertex, max_weight) <= DAY_IN_MINUTES;
+}
+
 
 int main() {
     Graph graph;
-    vector<int> bandwidths;
-    unsigned N, M;
+    vector<int> weights;
+
     cin >> N >> M;
-    for(unsigned i=0; i<M; i++){
+    for (unsigned i = 0; i < M; i++) {
         int v1, v2, t, b;
         cin >> v1 >> v2 >> t >> b;
         graph[make_edge(v1, v2)] = {t, b};
-        bandwidths.push_back(b);
+        graph[make_edge(v2, v1)] = {t, b};
+        weights.push_back(b);
     }
-    sort_and_unique(bandwidths);
+    if (N == 1 and M == 0) {
+        cout << 10000000;
+        return 0;
+    }
+    sort_and_unique(weights);
 
     unsigned left = 0;
-    unsigned right = bandwidths.size() - 1;
-    unsigned currentWeightIndex = bandwidths.size() / 2;
+    unsigned right = weights.size() - 1;
+    unsigned currentWeightIndex = weights.size() / 2;
     unsigned previousWeightIndex;
-    while(true){
-        if (IfPathExists(graph, N, bandwidths[currentWeightIndex])){
+    while (true) {
+        if (IfPathExists(graph, N, weights[currentWeightIndex])) {
             left = currentWeightIndex;
             previousWeightIndex = currentWeightIndex;
             currentWeightIndex = ceil((right + left) / 2.0);
             if (previousWeightIndex == currentWeightIndex) {
                 break;
             }
-        }else{
+        } else {
             right = currentWeightIndex;
             previousWeightIndex = currentWeightIndex;
             currentWeightIndex = ceil((right + left) / 2.0);
             if (previousWeightIndex == currentWeightIndex) {
+                if (currentWeightIndex != weights.begin()){
+
+                }
                 currentWeightIndex = currentWeightIndex - 1;
                 break;
             }
         }
     }
-    if (currentWeightIndex >= 0 and bandwidths[currentWeightIndex]-3000000 > 0) {
-        cout << floor((bandwidths[currentWeightIndex] - 3000000) / 100);
-    } else{
+    if (currentWeightIndex >= 0 and weights[currentWeightIndex] - 3000000 > 0) {
+        cout << floor((weights[currentWeightIndex] - 3000000) / 100);
+    } else {
         cout << 0;
     }
 
